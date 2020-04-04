69ce8c43b3d3403df00aa7d1ac63dc566984e60a
Zachary Lake <zlake23@gmail.com>
2020-04-04 10:44:45 -0700

    Reconfigured apply_template function with jinja

--- build.py
+++ build.py
@@ -4,8 +4,9 @@ import os
 from jinja2 import Template
 
 all_html_files = glob.glob('content/*.html')
-
 pages = []
+
+
 def page_list():
     for item in all_html_files:
         file_path = os.path.basename(item)
@@ -17,15 +18,16 @@ def page_list():
         })
 
 
-index_html = open("content/index.html").read()
-template_html = open("templates/base.html").read()
-template = Template(template_html)
-result = template.render({
-    "title": "Homepage",
-    "content": index_html,
-    page['active']: "active",
-})
-print(result)
+# index_html = open("content/index.html").read()
+# template_html = open("templates/base.html").read()
+# template = Template(template_html)
+# result = template.render({
+#     "title": "Homepage",
+#     "content": index_html,
+#     page['active']: "active",
+# })
+# print(result)
+
 # pages = [
 #     {
 #         "filename": "content/index.html",
@@ -57,33 +59,36 @@ print(result)
 
 # apply_template function will read in base template and
 # replace content and title string with code from each html page
-# def apply_template(content, title, active):
-#     template = open('./templates/base.html').read()
-#     template = template.replace("{{content}}", content)
-#     template = template.replace("{{title}}", title)
-    # template = template.replace("{{" + active + "}}", "active")
-#     return template
+def apply_template(content, page, active):
+    template_html = open("templates/base.html").read()
+    template = Template(template_html)
+    result = template.render({
+        "title": "Homepage",
+        "content": content,
+        page['active']: "active",
+    })
+    return result
 
-# # build function will write the completed webpage 
-# # to the docs directory
-# def build(doc, page):
-#         finished_doc = open(page['output'], 'w+').write(doc)
-#         return finished_doc
+# build function will write the completed webpage 
+# to the docs directory
+def build(doc, page):
+        finished_doc = open(page['output'], 'w+').write(doc)
+        return finished_doc
 
-# # main function will read in content from pages list
-# # build pages with apply_template and write to docs directory with build
-# def main():
-#     for page in pages:
-#         content = open(page['filename']).read()
-#         title = page['title']
-#         active = page['active']
-#         print('Reading:', page['filename'])
-#         doc = apply_template(content, title, active)
-#         print('Compiling template...')
-#         finished_webpage = build(doc, page)
-#         print('Writing to docs:', page['output'])
-#     print('[Website built]')
+# main function will read in content from pages list
+# build pages with apply_template and write to docs directory with build
+def main():
+    page_list()
+    for page in pages:
+        content = open(page['filename']).read()
+        active = page['active']
+        print('Reading:', page['filename'])
+        doc = apply_template(content, page, active)
+        print('Compiling template...')
+        finished_webpage = build(doc, page)
+        print('Writing to docs:', page['output'])
+    print('[Website built]')
 
-# # invoke main function to run build script       
-# if __name__ == "__main__":
-#     main()
+# invoke main function to run build script       
+if __name__ == "__main__":
+    main()
