3b0ec8d448a68120885b388d3df94b5899155839
Zachary Lake <zlake23@gmail.com>
2020-04-06 10:59:46 -0700

    Adds code to use sys for building website

--- manage.py
+++ manage.py
@@ -1,3 +1,16 @@
 import utils
-if __name__ == "__main__":
+import sys
+
+
+command = sys.argv[1]
+
+if command == "build":
+    print("Build was specified")
     utils.main()
+elif command == "new":
+    print("New page was specified")
+else:
+    print("Please specify 'build' or 'new'")
+
+# if __name__ == "__main__":
+#     utils.main()
