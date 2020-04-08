c0f4ada26c8e0f8be7ee84bb8147a120fcfae5f4
Zachary Lake <zlake23@gmail.com>
2020-04-07 17:32:12 -0700

    Updated utils build command

--- manage.py
+++ manage.py
@@ -5,12 +5,10 @@ import sys
 command = sys.argv[1]
 
 if command == "build":
-    print("Build was specified")
-    utils.main()
+    print("[Build was specified]")
+    if __name__ == "__main__":
+        utils.main()
 elif command == "new":
     print("New page was specified")
 else:
     print("Please specify 'build' or 'new'")
-
-# if __name__ == "__main__":
-#     utils.main()
